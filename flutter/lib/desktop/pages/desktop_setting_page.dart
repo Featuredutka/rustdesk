006457e6d6011e2421986a2524c5f39e02a8571e
Andrei Shingirii <andre@breitenstein.it>
2024-04-05 12:26:27 +0200

    Window size fix adjustments, UI minor updates

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2352,7 +2352,7 @@ class _AboutState extends State<_About> {
                         style: linkStyle,
                       ).marginSymmetric(vertical: 4.0)),
                   Container(
-                    decoration: const BoxDecoration(color: Color(0xFF2c8cff)),
+                    decoration: const BoxDecoration(color: Color(0xffffc800)),
                     padding:
                         const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
                     child: SelectionArea(
@@ -2364,13 +2364,13 @@ class _AboutState extends State<_About> {
                             children: [
                               Text(
                                 'Copyright © ${DateTime.now().toString().substring(0, 4)} Breitenstein IT.\n$license',
-                                style: const TextStyle(color: Colors.white),
+                                style: const TextStyle(color: Colors.black),
                               ),
                               Text(
                                 translate('Slogan_tip'),
                                 style: TextStyle(
                                     fontWeight: FontWeight.w800,
-                                    color: Colors.white),
+                                    color: Colors.black),
                               )
                             ],
                           ),
