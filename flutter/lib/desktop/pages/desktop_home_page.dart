c768314f5b00780207441daec782e319ec79ed98
Andrei Shingirii <andre@breitenstein.it>
2026-01-23 16:05:33 +0100

    Desktop page reconfiguration

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -85,22 +85,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
       if (bind.isCustomClient())
         Align(
           alignment: Alignment.center,
-          child: MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: () {
-                launchUrl(Uri.parse('https://breitenstein.it/'));
-              },
-              child: Opacity(
-                  opacity: 0.5,
-                  child: Text(
-                    translate("powered_by_me"),
-                    overflow: TextOverflow.clip,
-                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                        fontSize: 9, decoration: TextDecoration.underline),
-                  )),
-            ),
-          ).marginOnly(top: 6),
+          child: loadPowered(context),
         ),
       Align(
         alignment: Alignment.center,
@@ -485,13 +470,12 @@ class _DesktopHomePageState extends State<DesktopHomePage>
           bind.mainGotoInstall();
         });
       } else if (bind.mainIsInstalledLowerVersion()) {
-        // return buildInstallCard(
-        //     "Status", "Your installation is lower version.", "Click to upgrade",
-        //     () async {
-        //   await rustDeskWinManager.closeAllSubWindows();
-        //   bind.mainUpdateMe();
-        // });
-        
+        return buildInstallCard(
+            "Status", "Your installation is lower version.", "Click to upgrade",
+            () async {
+          await rustDeskWinManager.closeAllSubWindows();
+          bind.mainUpdateMe();
+        });
       }
     } else if (isMacOS) {
       final isOutgoingOnly = bind.isOutgoingOnly();
